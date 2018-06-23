433078bf34c199c02d88079d5de076b51c70b297
a1852rw <a1852rw@aiit.ac.jp>
2018-06-23 01:22:15 +0000

    error is happen. now

--- lesson04/005_wc.py
+++ lesson04/005_wc.py
@@ -9,20 +9,20 @@ n = len(words)
 d = {}
 
 for w in words:
-    if w id d:
-        d[w] + = 1
+    if w in d:
+        d[w] += 1
     else:
         d[w] = f
 
 def foo(s):
     return d[s]
-sorted_keys = sorted(d.keys(), key=foo, reverse=True)
+#sorted_keys = sorted(d.keys(), key=foo, reverse=True)
 
-sorted_keys = sorted(d.keys(),key = lanmbda x: d[x], reverse=True)
+sorted_keys = sorted(d.keys(), key = lanmbda x: d[x], reverse = True)
 
 i = 0
 for k in sorted_keys:
-    if i == 20
+    if i == 20:
      break
     print("{}: {}".format(k, d[k]))
     i += 1
