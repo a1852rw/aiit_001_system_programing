3ff9798ba528f2d0b8d3659869d729227ae2e8bd
a1852rw <a1852rw@aiit.ac.jp>
2018-06-25 12:49:20 +0000

    rewrite and delete lamda

--- lesson04/005_wc.py
+++ lesson04/005_wc.py
@@ -12,13 +12,13 @@ for w in words:
     if w in d:
         d[w] += 1
     else:
-        d[w] = f
+        d[w] = 1
 
 def foo(s):
     return d[s]
 #sorted_keys = sorted(d.keys(), key=foo, reverse=True)
 
-sorted_keys = sorted(d.keys(), key = lanmbda x: d[x], reverse = True)
+sorted_keys = sorted(d.keys(), key = lambda x: d[x], reverse = True)
 
 i = 0
 for k in sorted_keys:
