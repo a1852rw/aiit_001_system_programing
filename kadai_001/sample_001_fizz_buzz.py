2bc242f30ac781c27433ecaa9f1f9165d4321d17
a1852rw <a1852rw@aiit.ac.jp>
2018-06-26 11:51:42 +0000

    make one of sample not submit code

---
+++ kadai_001/sample_001_fizz_buzz.py
@@ -0,0 +1,13 @@
+i = 1
+while i < 101:
+    if i % 15 == 0:
+        print("Fizz Buzz!")
+    elif i % 3 == 0:
+        print("Fizz!")
+    elif i % 5 == 0:
+        print("Buzz!")
+    else:
+        print(i)
+
+    i += 1
+        
