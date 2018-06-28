d1626e6159887fb4bbf03244342f83bddd84ce3b
a1852rw <a1852rw@aiit.ac.jp>
2018-06-28 12:05:03 +0000

    add if code

--- kadai_001/sample_002_fizz_buzz.py
+++ kadai_001/sample_002_fizz_buzz.py
@@ -1,8 +1,14 @@
 #! /usr/bin/env python3
 
+def fb(i):
+    if i % 15 == 0: return "fizzbuzz"
+    elif i % 3 == 0: return "fizz"
+    elif i % 5 == 0: return "buzz"
+    else: return i
+
 i = 1
 while i <= 200:
     print(i,fb(i))
+    
     i = i + 1
 
-# ここから先は後で考えます。とりあえずここまでで保存することにします。
