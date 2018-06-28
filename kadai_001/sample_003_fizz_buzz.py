f697685088641b0c148e6c565a8c0f27aee17ebc
a1852rw <a1852rw@aiit.ac.jp>
2018-06-28 12:43:06 +0000

    kadai1-1 clear congratulationsgit add .

--- kadai_001/sample_003_fizz_buzz.py
+++ kadai_001/sample_003_fizz_buzz.py
@@ -2,12 +2,14 @@
 
 # 関数はできたのでif文をそうでないものに書き換える
 def fb(i):
-    if i % 15 == 0: return "fizzbuzz"
-    elif i % 3 == 0: return "fizz"
-    elif i % 5 == 0: return "buzz"
-    else: return i
+#    if i % 15 == 0: return "fizzbuzz"
+#    elif i % 3 == 0: return "fizz"
+#    elif i % 5 == 0: return "buzz"
+#    else: return i
 
-# とはいえどうしよう・・・・(´;ω;｀)
+# 比較演算子を使って置き換えることに成功
+
+    return (i % 3 == 0)*"fazz" + (i % 5 == 0)* "buzz"
 
 i = 1
 while i <= 200:
