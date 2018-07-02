d7e85e678b34e12ee4e15141d9596d3b9d4dda46
a1852rw <a1852rw@aiit.ac.jp>
2018-07-02 12:19:10 +0000

    first step copy and paste

--- kadai_001/kadai1-2.py
+++ kadai_001/kadai1-2.py
@@ -1,4 +1,19 @@
 #! /usr/bin/env python3
 
-#
-
+import sys
+i = 1
+if len(sys.argv) >= 2:
+    try:
+        f = open(sys.argv[1])
+        lines = f.readlines()
+        f.close()
+        for line in lines:
+            print i, line,
+            i += 1
+except IOError:
+        print 'Can not open file:', sys.argv[1], '\n'
+else:
+    while(True)
+        data = sys.stdin.readline()
+        print i, data,
+        i += 1
