3c12b5485be7f69a37cbea055a6e0afa444bd4ee
a1852rw <a1852rw@aiit.ac.jp>
2018-07-05 12:29:49 +0000

    エラーが発生したが一応ここでコミット

--- kadai_001/kadai1-2_nl.py
+++ kadai_001/kadai1-2_nl.py
@@ -1,26 +1,25 @@
 #! /usr/bin/env python3
 
-# とりあえず書くだけ書いてみます
-
 import sys
 i = 1
 if len(sys.argv) >= 2:
+# len関数は要素数を取得する。sys.argv変数はコマンドライン引数を取得する。len(sys.argv)で引数の数をカウントできる。
+# コマンドライン引数とは、コンピュータのコマンド入力画面(コマンドライン)からプログラムを起動する際に指定するパラメータのこと。プログラムの実行時にオプションや処理対象のデータなどを与えるために利用される
     try:
         f = open(sys.argv[1])
         lines = f.readlines()
         f.close()
         for line in lines:
-            print(i, line,)
-            i += 1
+            print(i, line)
+            i = i + 1
     except IOError:
         print('Can not open file:', sys.argv[1], '\n')
     else:
         while(True)
             data = sys.stdin.readline()
-            print(i, data,)
+            print(i, data)
             i += 1
 
-# だめでした
 # 要件をここに書いていきます
 # ⽬標: 20-25⾏程度
 # 要件1：os.system()でnlコマンドを起動する実装以外が望ましい。
