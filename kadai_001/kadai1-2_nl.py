aaeffa1591c8667d5222a6af6bebb10b615208d0
a1852rw <a1852rw@aiit.ac.jp>
2018-07-05 12:09:01 +0000

    文法上あきらかに間違っていた個所を修正しました。実行結果をみてさらに書き換えて行きます。

--- kadai_001/kadai1-2_nl.py
+++ kadai_001/kadai1-2_nl.py
@@ -1,5 +1,7 @@
 #! /usr/bin/env python3
 
+# とりあえず書くだけ書いてみます
+
 import sys
 i = 1
 if len(sys.argv) >= 2:
@@ -8,12 +10,18 @@ if len(sys.argv) >= 2:
         lines = f.readlines()
         f.close()
         for line in lines:
-            print i, line,
+            print(i, line,)
+# Print関数に()がついてませんでした、ここで追加します
+
             i += 1
     except IOError:
-        print 'Can not open file:', sys.argv[1], '\n'
+        print( 'Can not open file:', sys.argv[1], '\n')
+# ここも()がついていません、追加します
     else:
         while(True)
             data = sys.stdin.readline()
-            print i, data,
+            print(i, data,)
+# ここも()がついていません追加します
+# それぞれ中身は変数なので”などはいらないはずです。
             i += 1
+# これで保存して実行してみましょう
