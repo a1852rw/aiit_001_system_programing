31cd596fad727c6a2f966f0060597dc8e220a6df
a1852rw <a1852rw@aiit.ac.jp>
2018-07-15 23:03:28 +0900

    計算量の多いプログラム完成

--- kadai_002/sample_001_cal.py
+++ kadai_002/sample_001_cal.py
@@ -1,12 +1,13 @@
 #! usr/bin/env python3
 
-x = 1
+x = 0
 count = 1
 
-while (x < 1000000000000)
-	x = (x +1) * x
+while (count < 10000):
+	count = count + 1
+	x = (x + 1) * x
 
-print "計算結果を出力" +　x 
+print("計算結果を出力" +　x)
 
 # basshのtimeコマンドを使っての実行時間を計算するプログラムを作ってみる
 # とりあえず無駄に処理の重そうなコードを書いてみた
