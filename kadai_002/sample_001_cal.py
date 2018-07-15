83f1d66029d9205b8641e9050e675f950ac22340
a1852rw <a1852rw@aiit.ac.jp>
2018-07-16 01:15:06 +0900

    5分くらいで計算が終わるよう調整

--- kadai_002/sample_001_cal.py
+++ kadai_002/sample_001_cal.py
@@ -1,13 +1,14 @@
 #! usr/bin/env python3
 
-x = 0
+x = 1
 count = 1
 
-while (count < 10000):
+while (count < 25):
 	count = count + 1
 	x = (x + 1) * x
 
-print("計算結果を出力" +　x)
+print("計算結果を出力")
+print(x)
 
 # basshのtimeコマンドを使っての実行時間を計算するプログラムを作ってみる
 # とりあえず無駄に処理の重そうなコードを書いてみた
