05fc761f0b0a47e89f98caf1e270f6ac5b06ae3e
a1852rw <a1852rw@aiit.ac.jp>
2018-07-16 08:41:22 +0900

    コメントを追加

---
+++ kadai_002/sample_002_python/sample_001_cal.py
@@ -0,0 +1,14 @@
+#! usr/bin/env python3
+
+x = 1
+count = 1
+
+while (count < 25):
+	count = count + 1
+	x = (x + 1) * x
+
+print("計算結果を出力")
+print(x)
+
+# basshのtimeコマンドを使っての実行時間を計算するプログラムを作ってみる
+# とりあえず無駄に処理の重そうなコードを書いてみた。繰り返しは25回くらいが限度と思われる。
