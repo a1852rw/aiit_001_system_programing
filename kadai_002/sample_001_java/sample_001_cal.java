5ad4537af989f97d7a7afac1dcffb8f279141464
a1852rw <a1852rw@aiit.ac.jp>
2018-07-16 03:19:49 +0000

    change java code a little

--- kadai_002/sample_001_java/sample_001_cal.java
+++ kadai_002/sample_001_java/sample_001_cal.java
@@ -1,7 +1,7 @@
 public class sample_001_cal {
 	public static void main (String[] args){
-		int x = 1;
-		int count = 1;
+		long x = 1;
+		long count = 1;
 		
 		while(count < 25) {
 			count = count + 1;
@@ -11,8 +11,11 @@ public class sample_001_cal {
 		System.out.println("計算結果を出力");
 		System.out.println(x);
 		
+                System.out.println("繰り返し回数を出力");
+                System.out.println(count);		
 	}
 	
 }
 
 // pythonで作ったサンプルをJAVAで書き換えた
+// 整数型がintだったのに気づきlong型に変更した
