f6bddd93616e480a312eb31d1ecab7d015a9ac80
a1852rw <a1852rw@aiit.ac.jp>
2018-07-16 03:10:22 +0000

    コードに繰り返し回数のカウントや、変数型の書き換えなどを行った

--- kadai_002/sample_002_python/sample_002_cal.py
+++ kadai_002/sample_002_python/sample_002_cal.py
@@ -1,12 +1,18 @@
 #! usr/bin/env python3
 
 x = 1
+count = 1
 
 while (x < 1000000000):
-	x = x + 1
+        x = x + 1
+        count = count ; 1
 
 print("計算結果を出力")
 print(x)
+
+print("繰り返し回数を出力")
+print(count)
+
 
 # basshのtimeコマンドを使っての実行時間を計算するプログラムを作ってみる
 # 1億回足し算を繰り返すよう設定
