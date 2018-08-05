af3eed0227959777651df211952fec750d00d189
a1852rw <a1852rw@aiit.ac.jp>
2018-08-05 20:33:20 +0900

    とりあえず動いたことは動いたのでここで保存する

--- kadai_003/kadai3-3_mush.py
+++ kadai_003/kadai3-3_mush.py
@@ -1,8 +1,29 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
+import sys
 
+builtin = ['cd', 'exit']
 
+def execCd(cmd):
+        dir = cmd[3:]
+        try:
+                os.chdir(dir)
+        except:
+                print("Directory " + dir + " not found")
+
+def execExit():
+        sys.exit()
+
+while True:
+        cmd = input("mysh>")
+        if cmd.startswith("cd"):
+                execCd(cmd)
+        elif cmd.startswith("exit"):
+                execExit()
+        else:
+                os.system(cmd)
 
 # お題：以下の仕様のシェル（コマンドインタプリタ）を作成する
 
