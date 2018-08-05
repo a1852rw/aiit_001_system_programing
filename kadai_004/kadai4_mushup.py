ea461640bdc4327a3e8e78ebceff12605848fb61
a1852rw <a1852rw@aiit.ac.jp>
2018-08-05 23:57:16 +0900

    一部動作のコメントアウトを解除

--- kadai_004/kadai4_mushup.py
+++ kadai_004/kadai4_mushup.py
@@ -11,13 +11,12 @@ import json
 d = feedparser.parse("http://rss.weather.yahoo.co.jp/rss/days/4410.xml")
 
 
-# print("feed:", d.channel.title)
-# print("description:", d.channel.description)
+print("feed:", d.channel.title)
+print("description:", d.channel.description)
  
 for e in d.entries:
 	print("{}: {}".format(e.title, e.link))
-
-post = "{}: {}".format(e.title, e.link)
+	post = "{}: {}".format(e.title, e.link)
 
 SLACK_POST_URL = "https://hooks.slack.com/services/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"
 
