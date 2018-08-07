92af22eb19bff3053af6176dd542c0e12f96dcb4
a1852rw <a1852rw@aiit.ac.jp>
2018-08-07 08:59:55 +0000

    動作試験中、一回ここで保存します

--- exam_002/dict_003.py
+++ exam_002/dict_003.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-dict = {"alice": 3, "Bob": 5, "Dave": 4, "Victor": 10}
+dict = {"alice":3, "Bob":5, "Dave":4, "Victor":10}
 
-sorted_dict = sorted(dict.items(), 「空欄(a)」)
+sorted_dict = sorted(dict.items(), key=lambda x:x[0])
+
+print(len(dict.items()))
 
 for (k, v) in sorted_dict:
         print(k, "->" , v)
+#        print(len(k))
 
-# 辞書のキー(名前)が短い順に表示するコード(a)を考える。
+# 辞書のキー(名前)が短い順に表示するコード(a)を考える
