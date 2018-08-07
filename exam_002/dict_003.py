93c7694e43ceb5f9d44502c3b927f4b761d1e4d1
a1852rw <a1852rw@aiit.ac.jp>
2018-08-07 08:01:54 +0000

    ファイルを作ったところで一旦保存

---
+++ exam_002/dict_003.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+dict = {"alice": 3, "Bob": 5, "Dave": 4, "Victor": 10}
+
+sorted_dict = sorted(dict.items(), 「空欄(a)」)
+
+for (k, v) in sorted_dict:
+        print(k, "->" , v)
+
+# 辞書のキー(名前)が短い順に表示するコード(a)を考える。
