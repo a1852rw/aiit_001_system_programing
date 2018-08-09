17c23ad1e0669dd2bfdaf6ee2ebe4e0a2d8e0148
a1852rw <a1852rw@aiit.ac.jp>
2018-08-10 00:20:12 +0900

    正常動作を確認、不要なコメントを削除した

--- exam_002/dict_003.py
+++ exam_002/dict_003.py
@@ -5,10 +5,7 @@ dict = {"alice":3, "Bob":5, "Dave":4, "Victor":10}
 
 sorted_dict = sorted(dict.items(), key=lambda x:len(x[0]))
 
-# print(len(dict.items()))
-
 for (k, v) in sorted_dict:
         print(k, "->" , v)
-#        print(len(k))
 
 # 辞書のキー(名前)が短い順に表示するコード(a)を考える
