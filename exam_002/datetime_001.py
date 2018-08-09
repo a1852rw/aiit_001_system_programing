1240b70cc5e12dcc519f7ea3434f4a31974c3f5e
a1852rw <a1852rw@aiit.ac.jp>
2018-08-09 21:16:22 +0900

    要件と現在日時を取得するオブジェクトを書き込んだ

---
+++ exam_002/datetime_001.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
+
+t1 = datetime.now()
+
+print = t
+
+# 要件1：CGIスクリプトをPythonで書く
+# 要件2：朝は「Good morning」など、現在の時間に従って挨拶を出力する
+# 補足：Google App EngineのWebapp2(wsgi)を使っても構わない
+
+
+
+
