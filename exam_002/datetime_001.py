6ed4e4c6784241e30c0482e99802742b33a69aa4
a1852rw <a1852rw@aiit.ac.jp>
2018-08-09 21:43:38 +0900

    時刻ごとのあいさつの出力まで成功

--- exam_002/datetime_001.py
+++ exam_002/datetime_001.py
@@ -3,9 +3,24 @@
 
 from datetime import datetime
 
-t1 = datetime.now()
+t1 = datetime.now().strftime('%H')
+t2 = int(t1)
 
-print(t1)
+print(t2)
+
+
+
+if 4 < t2 < 9:
+	print("おはようございます")
+elif 9 <= t2 < 18:
+	print("こんにちは")	
+elif 18 <= t2 < 23:
+	print("こんばんは")	
+elif 23 <= t2 <= 24:
+	print("おやすみなさい")
+elif 0 <= t2 <= 4:
+	print("早く寝てください")
+
 
 # 要件1：CGIスクリプトをPythonで書く
 # 要件2：朝は「Good morning」など、現在の時間に従って挨拶を出力する
