641fc55cced600e379b2249040ac353bb66afe2a
a1852rw <a1852rw@aiit.ac.jp>
2018-08-13 21:49:35 +0900

    無事に出来上がり

--- lesson_001/005_fox.py
+++ lesson_001/005_fox.py
@@ -5,13 +5,13 @@ s1 = "The quick brown fox jumps over te lazy dog."
 s2 = s1[:3]
 print(s2)
 
-s2 = s1[16:18]
+s2 = s1[16:19]
 print(s2)
 
-s2 = s1[5:10] + s1[-4:-1]
+s2 = s1[4:10] + s1[-4:-1]
 print(s2)
 
-s2 = s1[:3] + s1[-9:-6] + s1[11:15] + s1[17:19] + s1[-1:]
+s2 = s1[:4] + s1[-9:-4] + s1[10:19] + s1[-1:]
 print(s2)
 
 
