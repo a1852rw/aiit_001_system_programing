fb8fd1d8799909daf30828d7bdaf03ee49d9dd40
a1852rw <a1852rw@aiit.ac.jp>
2018-08-15 22:23:42 +0900

    ファイル008の動作確認完了

--- lesson_001/008_first_last.py
+++ lesson_001/008_first_last.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+def first_last(s):
+	if len(s) <= 1:
+		return ""
+	else:
+		return s[:2] + s[-2:]
 
-
+print(first_last("spring"))
+print(first_last("hello"))
+print(first_last("a"))
+print(first_last("abc"))
 
 
 # 要件1；文字列を受け取って最初と最後の各2文字から作成した文字字列を返すfirst_last関数を書く
