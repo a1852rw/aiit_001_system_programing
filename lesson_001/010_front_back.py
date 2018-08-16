47fe172b036fe63d2e7773f252d88f13b5a4d248
a1852rw <a1852rw@aiit.ac.jp>
2018-08-16 21:51:21 +0900

    途中だが一回コミットする

--- lesson_001/010_front_back.py
+++ lesson_001/010_front_back.py
@@ -2,7 +2,15 @@
 # -*- coding: utf-8 -*-
 
 def front_back(a, b):
-	# ここに条件分岐と処理内容を書き込む
+	a1 = len(a)
+	b1 = len(b)
+	a2 = len(a) / 2
+	b2 = len(b) / 2
+	
+	return(a[a1])
+
+#	if a1 % 2 == 0 and b1 % 2 == 0:
+#		return a[: a2] + b[: b2] + a[-a2 :] + b[-b2 :]
 
 print(front_back('abcd', 'xy'))
 print(front_back('abcde', 'xyz'))
@@ -11,7 +19,7 @@ print(front_back('Kitten', 'Donut'))
 # 要件1：2つの文字列a及びbを受け取って、aの前＋bの前＋aの後＋bの後から構成される文字列を返すfront_back関数を書く
 # 要件2：受け取った文字列は2分割する。
 # 要件3：文字列の長さが偶数であれば真ん中で分割する
-# 要件4：文字列の長さが奇数であれば、前の部分⽂字列を1文字多くする。
+# 要件4：文字列の長さが奇数であれば、前の部分文字列を1文字多くする。
 
 # 出力1：abxcdy
 # 出力2：abcxydez
