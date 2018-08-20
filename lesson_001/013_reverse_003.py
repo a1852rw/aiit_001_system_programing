f60aedc186040d29ef04f7e74f45fb93b62588e2
a1852rw <a1852rw@aiit.ac.jp>
2018-08-20 20:30:17 +0900

    ファイル013の作成と動作確認まで完了

--- lesson_001/013_reverse_003.py
+++ lesson_001/013_reverse_003.py
@@ -2,13 +2,16 @@
 # -*- coding: utf-8 -*-
 
 def reserve(s):
-# ここに何か書く(おそらくは文字列を反転させるコード)
+	return str(s[::-1])
 
 orig = input("Type a phrease: ")
-result = reverse(orig)
-# ここに回文かどうかを判定するプログラムを書く
+result = reserve(orig)
 
-
+if orig == result:
+	print("** palindrome **")
+# 文字列が反転前/後で同じ値になる=回文である
+elif orig != result:
+	print(result)
 
 # 要件1：reverse関数を作成する
 # 要件2：キーボードから受け取った文字列が回文の場合「** palindrome **」と出力する
