5a96c84a240ffa4509528839516e46c918d20c9f
a1852rw <a1852rw@aiit.ac.jp>
2018-08-20 20:19:45 +0900

    とりあえず問題文13のファイルを作ってコメントを入力した

---
+++ lesson_001/013_reverse_003.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def reserve(s):
+# ここに何か書く(おそらくは文字列を反転させるコード)
+
+orig = input("Type a phrease: ")
+result = reverse(orig)
+# ここに回文かどうかを判定するプログラムを書く
+
+
+
+# 要件1：reverse関数を作成する
+# 要件2：キーボードから受け取った文字列が回文の場合「** palindrome **」と出力する
+# 要件3：キーボードから受け取った文字列が回文ではない場合、受け取った文字列を反転して出力する
+
+# 入力1：alice
+# 出力1：ecila
+
+# 入力2：anna
+# 出力2：** palindrome **
