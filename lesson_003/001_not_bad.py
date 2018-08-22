7ec94e1ed9100b19e78a2a9f2c3d078b183a25a2
a1852rw <a1852rw@aiit.ac.jp>
2018-08-22 23:33:45 +0900

    要件と与えられたコードを書き写した

---
+++ lesson_003/001_not_bad.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding; utf-8 -*-
+
+def not_bad(s):
+# ここに要件を満たす処理を記述する
+
+
+print(not_bad('This movie is not so bad'))
+print(not_bad('This dinner is not that bad!'))
+print(not_bad('This tea is not hot'))
+print(not_bad("It's bad yet not"))
+
+
+# 要件1：not_bad関数を作成する
+# 要件2：受け取った文字列の中の「not」の後に「bad」が現れたら「not」から「bad」までの文字列を「good」で置換する
+
+# 出力1：This movie is good
+# 出力2：This dinner is good!
+# 出力3：This tea is not hot
+# 出力4：It's bad yet not
+
+
