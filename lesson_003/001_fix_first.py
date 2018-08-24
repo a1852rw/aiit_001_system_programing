7cadd0eda6931adcbc3898338b33aa013968f791
a1852rw <a1852rw@aiit.ac.jp>
2018-08-24 22:10:58 +0900

    要件1と要件2を満たしたところで一度保存

--- lesson_003/001_fix_first.py
+++ lesson_003/001_fix_first.py
@@ -1,2 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding; utf-8 -*-
+
+def fix_first(s):
+	s1 = s[:1]
+	return s.replace(s1,"*")
+
+print(fix_first("babble"))
+print(fix_first("google"))
+print(fix_first("apple"))
+print(fix_first("orange"))
+
+
+# 要件1：fix_first関数を記述する
+# 要件2：文字列を受け取り最初の文字と同じ文字を'*'に変更して返却する
+# 要件3：最初の文字は変更しない
+
+# 出力1：ba**le
+# 出力2：goo*le
+# 出力3：apple
+# 出力4：orange
+
