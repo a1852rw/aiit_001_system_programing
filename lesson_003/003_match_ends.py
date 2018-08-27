b92a614d870aa65ec9be2537c7e867c89fffd283
a1852rw <a1852rw@aiit.ac.jp>
2018-08-27 21:39:57 +0900

    ループ処理で要素数を計算するなど間違ってる気がする。しかし動作は正常なのでコミット

--- lesson_003/003_match_ends.py
+++ lesson_003/003_match_ends.py
@@ -1,26 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""
 def match_ends(li):
+	i = 0
 	for s1 in li:
-		return len(s1)
+		if len(s1) >= 2 and s1[:1] == s1[-1:]:
+			i = i + 1
+	return i
 
 print(match_ends(['aba', 'xyz', 'aa', 'x', 'bbb']))
 print(match_ends(['', 'x', 'xy', 'xyx', 'xx']))
 print(match_ends(['aaa', 'be', 'abc', 'hello']))
-"""
 
-li = ["aba","xyz","aa","bbb"]
-i = 0
-
-for s1 in li:
-	if len(s1) >= 2 and s1[:1] == s1[-1:]:
-		i = i + 1
-#		print(s1)
-#		print(i)
-		
-print("合計：" + str(i))
 
 # 要件1：関数match_endsを書く
 # 要件2：受け取った文字列のリストの要素のうち、2文字以上で、最初と最後が同じ文字の文字列の数を返す
