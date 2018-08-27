73f45a156daf3d491a41a05b630e9781ac86cc26
a1852rw <a1852rw@aiit.ac.jp>
2018-08-27 21:00:08 +0900

    正しい文字列を抽出しその文字数を出力するとこまで成功

--- lesson_003/003_match_ends.py
+++ lesson_003/003_match_ends.py
@@ -15,7 +15,7 @@ li = ["aba","xyz","aa","bbb"]
 
 for s1 in li:
 	if len(s1) >= 2 and s1[:1] == s1[-1:]:
-		print(s1)
+		print(len(s1))
 
 # 要件1：関数match_endsを書く
 # 要件2：受け取った文字列のリストの要素のうち、2文字以上で、最初と最後が同じ文字の文字列の数を返す
