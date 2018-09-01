41676261c39eb52554721e23c92b5ef60b0b27f8
a1852rw <a1852rw@aiit.ac.jp>
2018-09-01 13:17:39 +0000

    予定していた動作をすることを確認した

--- lesson_003/004_front_x.py
+++ lesson_003/004_front_x.py
@@ -2,34 +2,25 @@
 # -*- coding; utf-8 -*-
 
 def front_x(x):
-	for s1 in x:
-#		if s1[:1] == "x":
-#			s2 = s1 
-#		else:
-#			s2 = s1		
-#	return s2
-		s2 = s1
-		s1 = s1 + " " + s2
+	s1 = []
+	s2 = []
+# 	まずは空のリストs1,s2を作成する
+
+	for s in x:
+		if s.startswith("x"):
+#		対象の文字列がXで始まるとき
+			s1.append(s)
+#			対象の文字列をリストs1に追加する
+		else:
+			s2.append(s)
+#			それ以外の場合は対象の文字列をリストs2に追加する
 		
-	return s1	
+	return sorted(s1) + sorted(s2)
+#	リストs1とリストs2を連結したリストを返却する
 
 print(front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa']))
 print(front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa']))
 print(front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark']))
-
-
-# def front_x(x):
-#    l1, l2 = [], []
-#    for e in x:
-#        if e.startswith('x'):
-#            l1.append(e)
-#        else:
-#            l2.append(e)
-#    return sorted(l1) + sorted(l2)
-
-
-
-
 
 # 要件1：front_x関数を記述する
 # 要件2：この関数は受け取った文字列のリストの要素を分類する「x」で始まる文字列と「x以外」で始まる文字列に分類する
