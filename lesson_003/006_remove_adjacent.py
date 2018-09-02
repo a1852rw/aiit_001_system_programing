5aaac66e2ea6150e0bbe0beeda7f65de7b3513aa
a1852rw <a1852rw@aiit.ac.jp>
2018-09-02 08:19:46 +0000

    要件を書き込んだ

--- lesson_003/006_remove_adjacent.py
+++ lesson_003/006_remove_adjacent.py
@@ -1,2 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding; utf-8 -*-
+
+def remove_adjacent(li):
+    # ここに対応する処理を記述する
+    
+print(remove_adjacent([1,2,2,3]))	
+print(remove_adjacent([2,2,3,3,3]))	
+print(remove_adjacent([]))
+
+# 要件1：remove_adjacent関数を記述する
+# 要件2：リストを受け取り隣接した要素が同じだった場合にその要素を削除する
+# 要件3：処理の後に値を返却する
+
+# 出力1：[1,2,3]
+# 出力2：[2,3]
+# 出力3：[]
