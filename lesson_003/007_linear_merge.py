db5d3d112329022b774e038ead45ac0fd722433e
a1852rw <a1852rw@aiit.ac.jp>
2018-09-02 13:31:12 +0000

    要件を書き込んだ

--- lesson_003/007_linear_merge.py
+++ lesson_003/007_linear_merge.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding; utf-8 -*-
+
+def linear_merge(li1,li2):
+    # 処理の内容を記述する
+    
+print(linear_merge(['aa','xx','zz'],['bb','cc']))	
+print(linear_merge(['aa','xx'],['bb','cc','zz']))	
+print(linear_merge(['aa','aa'],['aa','bb','bb']))
+
+# 要件1：関数lier_mergeを記述する
+# 要件2：昇順(⼩さい順)の2つのリストを受け取る
+# 要件3：マージして要素の値で昇順で整列したリストを返却する
+
+# 出力1：['aa','bb','cc','xx','zz']	
+# 出力2：['aa','bb','cc','xx','zz']	
+# 出力3：['aa','aa','aa','bb','bb']
+
