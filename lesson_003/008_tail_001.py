21dfea01feada3855ce69d6a3aea8e8c59c87280
a1852rw <a1852rw@aiit.ac.jp>
2018-09-03 11:18:53 +0000

    まず要件を入力したところでコミット

--- lesson_003/008_tail_001.py
+++ lesson_003/008_tail_001.py
@@ -1,2 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding; utf-8 -*-
+
+f = open('small.txt','rU')	
+#ここに何がしかのコードを入力する
+      
+f.close()
+
+
+# 要件1：ファイル「small.txt」を読み込む
+# 要件2：読み込んだファイルを後ろの⾏から表⽰する
+# 注：ファイル「small.txt」は同じディレクトリに保存してある
+
+'''
+出力する文字列
+-- Football Coach	
+But at least we	are not	what we	used to be
+We are not what	we need	to be
+We are not what	we should be
+'''
