f0d1a91d7aa79e121cff0d85e1d2bfe5f0a27dff
a1852rw <a1852rw@aiit.ac.jp>
2018-09-03 12:04:47 +0000

    要件を入力した

--- lesson_003/009_grep_001.py
+++ lesson_003/009_grep_001.py
@@ -1,2 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding; utf-8 -*-
+
+f = open('small.txt','rU')	
+pat = "but"	
+# ここに何がしかのコードを入力する
+	
+f.close()
+
+# 要件1：ファイルを読み込み指定された⽂字列が存在する⾏だけを出力する
+# 要件2：⼤⽂字⼩⽂字を無視する
+# 要件3：「要件1」とは別に指定された文字列を「***」で強調し出力する
+
+# 出力1：But at least we are not what we used to be
+# 出力2：***But*** at least we are not what we used to be
