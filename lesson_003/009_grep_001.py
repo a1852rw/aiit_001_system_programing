157e324c6c28eb4feec957ad95e3cf9ac89a93bd
a1852rw <a1852rw@aiit.ac.jp>
2018-09-06 21:11:04 +0900

    ちょっと変えてみた

--- lesson_003/009_grep_001.py
+++ lesson_003/009_grep_001.py
@@ -3,13 +3,17 @@
 
 f = open('small.txt','rU')	
 pat = "but"	
-# ここに何がしかのコードを入力する
+
+f1 = list(f)
+f2 = [s for s in f1 if pat in s]
+print(f2)
 	
 f.close()
 
-# 要件1：ファイルを読み込み指定された⽂字列が存在する⾏だけを出力する
-# 要件2：⼤⽂字⼩⽂字を無視する
+# 要件1：ファイルを読み込み指定された文字列が存在する行だけを出力する
+# 要件2：大文字/小文字を無視する
 # 要件3：「要件1」とは別に指定された文字列を「***」で強調し出力する
+# 要件4：文字列は同じディレクトリにあるファイル「small.txt」から取得する
 
 # 出力1：But at least we are not what we used to be
 # 出力2：***But*** at least we are not what we used to be
