795ce06e9c1b48552577de472194646bab240541
a1852rw <a1852rw@aiit.ac.jp>
2018-09-10 20:40:33 +0900

    要件を入力した

--- lesson_003/010_tail_002.py
+++ lesson_003/010_tail_002.py
@@ -1,2 +1,31 @@
 #!/usr/bin/env python3
 # -*- coding; utf-8 -*-
+
+f = open('small.txt','rU')
+
+f1 = list(f)
+for line in reversed(f1):
+	print(line, end='')
+f.close()
+
+# 要件1：ファイル「small.txt」を読み込む
+# 要件2：読み込んだファイルを後ろの⾏から表⽰する
+# 注：ファイル「small.txt」は同じディレクトリに保存してある
+
+# 追加要件1：通常のUnixコマンドのように、ファイル名をコマンドライン引数で指定できるようにする。
+# 追加要件2：ファイル名が省略されたら、標準⼊⼒（stdin）から⼊⼒を受け取るようにする。
+
+
+'''
+出力する文字列
+-- Football Coach	
+But at least we	are not	what we	used to be
+We are not what	we need	to be
+We are not what	we should be
+'''
+
+'''
+コマンド入力例は下記の通り
+$ python tail.py small.txt
+$ cat small.txt | python tail.py
+'''
--- lesson_003/011_grep_002.py
+++ lesson_003/011_grep_002.py
@@ -1,2 +1,38 @@
 #!/usr/bin/env python3
 # -*- coding; utf-8 -*-
+
+f = open('small.txt','rU')	
+pat = "but"	
+
+import re
+
+f1 = list(f)
+f2 = re.compile(pat, re.IGNORECASE)
+
+for f3 in f1:
+	if re.search(f2, f3) != None:
+		print(f3)	
+
+		m1 = re.match(f2,f3)
+		f4 = re.sub(f2, "***" + str(f3[m1.start():m1.end()]) + "***", f3)
+		print(f4)	
+
+f.close()
+
+# 要件1：ファイルを読み込み指定された文字列が存在する行だけを出力する
+# 要件2：大文字/小文字を無視する
+# 要件3：「要件1」とは別に指定された文字列を「***」で強調し出力する
+# 要件4：文字列は同じディレクトリにあるファイル「small.txt」から取得する
+
+# 追加要件1：通常のUnixコマンドのように、ファイル名をコマンドライン引数で指定できるようにする。
+# 追加要件2：ファイル名が省略されたら、標準⼊⼒（stdin）から⼊⼒を受け取るようにする。
+# 追加要件3：パターン⽂字列を引数で渡すようにする。
+
+# 出力1：But at least we are not what we used to be
+# 出力2：***But*** at least we are not what we used to be
+
+'''
+コマンド入力例は下記の通り
+$ python grep.py but small.txt
+$ cat small.txt | python grep.py but
+'''
